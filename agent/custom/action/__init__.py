35cd80e016bf9560bb9077fc804f26b75cae6d64
yororoA <3364817735@qq.com>
2026-05-03 10:52:15 +0800

    chore

--- agent/custom/action/__init__.py
+++ agent/custom/action/__init__.py
@@ -3,10 +3,6 @@ from .AutoFish.auto_fish import *
 from .AutoFish.auto_buy_fish_bait import *
 from .AutoFish.auto_sell_fish import *
 from .auto_make_coffee import *
-from .auto_fish import *
-from .auto_buy_fish_bait import *
-from .auto_sell_fish import *
-from .click import *
 from .auto_rhythm import *
 from .rhythm.feats.select_song import *
 from .rhythm.feats.repeat import *
