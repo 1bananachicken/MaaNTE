592b553750816e02f7337c164aef099a6b5b4ec8
CCYellowStar2 <cc1147036991@gmail.com>
2026-05-06 10:58:19 +0800

    feat: 添加极速拾取功能 (F+滚轮) 的自定义动作与任务配置 (#103)
    * feat: 添加极速拾取功能 (F+滚轮) 的自定义动作与任务配置
    
    * 删掉奇怪的注释
    
    Remove commented code that indicates immediate success regardless of input.

--- agent/custom/action/__init__.py
+++ agent/custom/action/__init__.py
@@ -9,6 +9,7 @@ from .rhythm.feats.select_song import *
 from .Common.click import *
 from .realtime_task import *
 from .AutoFish.auto_fish_withoutCV import *
+from .auto_f_scroll import *
 
 __all__ = [
     "AutoFishNew",
@@ -22,4 +23,5 @@ __all__ = [
     "AutoRhythmSelectSong",
     "RealTimeTaskAction",
     "AutoFishWithoutCV",
+    "AutoFScroll",
 ]
