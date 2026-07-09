589a5377ec7097858a24e6ae2b734bc3158374e1
Lisadnessxin <1196859934@qq.com>
2026-07-09 21:44:38 +0800

    单人战斗，只能使用qe和闪避

--- agent/custom/action/__init__.py
+++ agent/custom/action/__init__.py
@@ -29,6 +29,8 @@ from .withdraw_money_choose_item import *
 from .SyncCharacterAbilityCityAbility import *
 from .DatasetCollection.autonomous_driving_dataset_recorder import *
 from .BagelSpam import *
+from .autofight import *
+
 
 __all__ = [
     "AutoMakeCoffee",
@@ -68,4 +70,5 @@ __all__ = [
     "BagelSpamPickIndex",
     "BagelSpamOutputText",
     "BagelSpamLLMGenerate",
+    "AF_AutoFightClsTest"
 ]
